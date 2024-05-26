dca0926c3a9660d5cf898fe3ccd0659f47064c74
Juan <juanignaciobdm@gmail.com>
2024-05-26 13:16:21 +0200

    pantallas: login,register,recuperar contraseña, detalle del producto; Faltan retoques

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { RecoverPassComponent } from './pages/recover-pass/recover-pass.component';
+import { DetailsComponent } from './pages/details/details.component';
 
 const routes: Routes = [
   {
@@ -27,6 +28,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'details',
+    component: DetailsComponent,
   },
 
 ];
